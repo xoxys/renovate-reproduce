b34fbad4e9a50754f226db5783ef890f5e695d66
Robert Kaussow <mail@thegeeklab.de>
2024-12-11 21:00:08 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/xoxys/renovate-reproduce
+
+go 1.23.1
+
+require github.com/google/go-github/v66 v66.0.0
+
+require github.com/google/go-querystring v1.1.0 // indirect
---
+++ internal/foo/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/go-github/v66/github"
+)
+
+func main() {
+	var allComments []*github.IssueComment
+
+	fmt.Println(allComments)
+}
